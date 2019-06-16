1e9bd9fd1e25c71b0b2f8dca977bdeaec2e94605
hussain <m.hussainul.islam@gmail.com>
2019-06-16 01:54:01 -0400

    TeamMembers component working

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,22 +1,36 @@
 <template>
     <div>
+        <b-card
+            :title="team.TeamName"
+            class="mb-2"
+        >
+            <b-card-text>
+
+
+            </b-card-text>
+
+        </b-card>
+
+
         <h2>{{team.TeamName}}</h2>
-        <projects 
-            :projects="team.Projects" 
-        />
-        <team-lead
-            :key="teamLead._id" 
-            :teamLead="teamLead"
-        />
+        <team-lead :teamLead="teamLead" />
+        <team-members :key="teamMembers._id" :teamMembers="teamMembers" />
+        <projects :projects="team.Projects" />
 
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import getEmployeeData from '../employees'
 
 import Projects from './Projects'
 import TeamLead from './TeamLead'
+import TeamMembers from './TeamMembers'
 
 export default {
     name: 'Team',
@@ -27,13 +41,15 @@ export default {
                 errored: false
             },
             employees: null,
-            teamLead: null            
+            teamLead: null,
+            teamMembers: []            
         }
     },
     props: [ 'team' ],
     components:{
         Projects,
-        TeamLead
+        TeamLead,
+        TeamMembers
     },
     created:function(){
         getEmployeeData()
@@ -42,6 +58,9 @@ export default {
             })
             .then(()=>{
                 this.teamLead = this.employees.find(employee=>employee._id == this.team.TeamLead)
+            })
+            .then(()=>{
+                this.team.Employees.forEach(member=>this.teamMembers.push(this.employees.find(employee=>employee._id == member)))
             })
             .catch(error=>{
                 this.errored = true;
--- src/components/TeamMembers.vue
+++ src/components/TeamMembers.vue
@@ -1,10 +1,21 @@
 <template>
+  <div>
+    <select name="team-members" id="team-membes">
+      <option 
+        v-for="member of teamMembers"
+        :key="member._id"
+        :member="member"
+        :value="member.FirstName"
+      >{{ member.FirstName }} {{ member.LastName }}</option>
+    </select>
+  </div>
     
 </template>
 
 <script>
 export default {
-
+  name: 'TeamMembers',
+  props: [ 'teamMembers' ],
 }
 </script>
 
