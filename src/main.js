a33545de3f1475360fa5fa1fdbf151e7aa6806c7
hussain <m.hussainul.islam@gmail.com>
2019-06-18 21:47:10 -0400

    ready for submission

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* WEB422 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+of this
+* assignment has been copied manually or electronically from any other source (including web sites)
+or
+* distributed to other students.
+*
+* Name: Md. Hussainul Islam Sajib Student ID: 137651170 Date: 18 June 2019
+*
+********************************************************************************/ 
+
 import Vue from 'vue'
 import App from './App.vue'
 
